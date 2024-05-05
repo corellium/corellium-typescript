00540c6bdf11f1cf94a9523cfce6b8609d9e36e0
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-04 23:57:43 -0400

    Create Media endpoints

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ import { createCustomNetworkEndpoints } from './custom-network';
 import { createModelEndpoints } from './model';
 import { createConsoleEndpoints } from './console';
 import { createPanicEndpoints } from './panic';
+import { createMediaEndpoints } from './media';
 import type { paths } from '../types/corellium';
 
 type CorelliumOptions = {
@@ -62,6 +63,7 @@ class Corellium {
   public hypertrace = this.api ? createHyperTraceEndpoints(this.api) : null;
   public kernelHook = this.api ? createKernelHookEndpoints(this.api) : null;
   public image = this.api ? createImageEndpoints(this.api) : null;
+  public media = this.api ? createMediaEndpoints(this.api) : null;
   public model = this.api ? createModelEndpoints(this.api) : null;
   public networkMonitor = this.api
     ? createNetworkMonitorEndpoints(this.api)
---
+++ src/media.ts
@@ -0,0 +1,42 @@
+import type createFetchClient from 'openapi-fetch';
+import type { paths } from '../types/corellium';
+
+export const createMediaEndpoints = (
+  api: ReturnType<typeof createFetchClient<paths>>
+) => ({
+  start: async (
+    instanceId: string,
+    body: paths['/v1/instances/{instanceId}/media/play']['post']['requestBody']['content']['application/json']
+  ) => {
+    const response = await api.POST('/v1/instances/{instanceId}/media/play', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+      body,
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  stop: async (instanceId: string) => {
+    const response = await api.POST('/v1/instances/{instanceId}/media/stop', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+});
