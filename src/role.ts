1e130309540da98aa430c2e96317f2674225f0ec
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-05 12:22:02 -0400

    Add role and snapshot annotations

--- src/role.ts
+++ src/role.ts
@@ -4,6 +4,12 @@ import type { paths } from '../types/corellium';
 export const createRoleEndpoints = (
   api: ReturnType<typeof createFetchClient<paths>>
 ) => ({
+  /**
+   * List all roles.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.role.list();
+   */
   list: async () => {
     const response = await api.GET('/v1/roles');
 
@@ -14,6 +20,18 @@ export const createRoleEndpoints = (
     return response.data;
   },
 
+  /**
+   * Add a role for a user or team to a project.
+   * @param projectId The project ID.
+   * @param roleId The role ID.
+   * @param options The user or team ID.
+   * @param options.userId The user ID.
+   * @param options.teamId The team ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.role.add('123', '456', { userId: '789' });
+   * @example const response = await corellium.role.add('123', '456', { teamId: '789' });
+   */
   add: async (
     projectId: paths['/v1/roles/projects/{projectId}/users/{userId}/roles/{roleId}']['put']['parameters']['path']['projectId'],
     roleId: paths['/v1/roles/projects/{projectId}/users/{userId}/roles/{roleId}']['put']['parameters']['path']['roleId'],
@@ -66,6 +84,18 @@ export const createRoleEndpoints = (
     return response.data;
   },
 
+  /**
+   * Delete a role for a user or team from a project.
+   * @param projectId The project ID.
+   * @param roleId The role ID.
+   * @param options The user or team ID.
+   * @param options.userId The user ID.
+   * @param options.teamId The team ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.role.delete('123', '456', { userId: '789' });
+   * @example const response = await corellium.role.delete('123', '456', { teamId: '789' });
+   */
   delete: async (
     projectId: paths['/v1/roles/projects/{projectId}/users/{userId}/roles/{roleId}']['delete']['parameters']['path']['projectId'],
     roleId: paths['/v1/roles/projects/{projectId}/users/{userId}/roles/{roleId}']['delete']['parameters']['path']['roleId'],
--- src/snapshot.ts
+++ src/snapshot.ts
@@ -5,6 +5,12 @@ export const createSnapshotEndpoints = (
   api: ReturnType<typeof createFetchClient<paths>>,
   instanceId?: string
 ) => ({
+  /**
+   * List all snapshots.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').snapshot.list();
+   */
   list: async () => {
     if (!instanceId) {
       throw new Error('instanceId is required');
@@ -25,6 +31,14 @@ export const createSnapshotEndpoints = (
     return response.data;
   },
 
+  /**
+   * Get a snapshot.
+   * @param snapshotId The snapshot ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.snapshot.get('123');
+   * @example const response = await corellium.device('123').snapshot.get('123');
+   */
   get: async (
     snapshotId: paths['/v1/snapshots/{snapshotId}']['get']['parameters']['path']['snapshotId']
   ) => {
@@ -63,6 +77,13 @@ export const createSnapshotEndpoints = (
     return response.data;
   },
 
+  /**
+   * Create a snapshot.
+   * @param body The snapshot data.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').snapshot.create({ name: 'Snapshot' });
+   */
   create: async (
     body: paths['/v1/instances/{instanceId}/snapshots']['post']['requestBody']['content']['application/json']
   ) => {
@@ -86,6 +107,14 @@ export const createSnapshotEndpoints = (
     return response.data;
   },
 
+  /**
+   * Delete a snapshot.
+   * @param snapshotId The snapshot ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.snapshot.delete('123');
+   * @example const response = await corellium.device('123').snapshot.delete('123');
+   */
   delete: async (
     snapshotId: paths['/v1/snapshots/{snapshotId}']['delete']['parameters']['path']['snapshotId']
   ) => {
@@ -124,6 +153,15 @@ export const createSnapshotEndpoints = (
     return response.data;
   },
 
+  /**
+   * Update a snapshot.
+   * @param snapshotId The snapshot ID.
+   * @param body The snapshot data.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.snapshot.update('123', { name: 'Snapshot' });
+   * @example const response = await corellium.device('123').snapshot.update('123', { name: 'Snapshot' });
+   */
   update: async (
     snapshotId: paths['/v1/snapshots/{snapshotId}']['patch']['parameters']['path']['snapshotId'],
     body: paths['/v1/instances/{instanceId}/snapshots/{snapshotId}']['patch']['requestBody']['content']['application/json']
@@ -165,6 +203,13 @@ export const createSnapshotEndpoints = (
     return response.data;
   },
 
+  /**
+   * Restore a snapshot.
+   * @param snapshotId The snapshot ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').snapshot.restore('123');
+   */
   restore: async (
     snapshotId: paths['/v1/instances/{instanceId}/snapshots/{snapshotId}/restore']['post']['parameters']['path']['snapshotId']
   ) => {
@@ -192,6 +237,16 @@ export const createSnapshotEndpoints = (
   },
 
   sharing: {
+    /**
+     * Share a snapshot.
+     * @param snapshotId The snapshot ID.
+     * @param body The sharing data.
+     * @param body.sharingType The sharing type e.g. `publicLink`, `domainRestrictedLink`, `passwordPublicLink` or `emailInvite.`.
+     * @param body.password The password for the sharing type `passwordPublicLink`.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.snapshot.sharing.create('123', { sharingType: 'publicLink' });
+     */
     create: async (
       snapshotId: paths['/v1/snapshots/{snapshotId}/share']['post']['parameters']['path']['snapshotId'],
       body: paths['/v1/snapshots/{snapshotId}/share']['post']['requestBody']['content']['application/json']
@@ -212,6 +267,15 @@ export const createSnapshotEndpoints = (
       return response.data;
     },
 
+    /**
+     * Allow access to a shared snapshot.
+     * @param snapshotId The snapshot ID.
+     * @param body The sharing data.
+     * @param body.members The members to allow access e.g. `['jane@acme.com', 'john@acme.com']`.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.snapshot.sharing.allow('123', { members: ['jane@acme.com', 'john@acme.com'] });
+     */
     allow: async (
       snapshotId: paths['/v1/snapshots/{snapshotId}/permissions']['post']['parameters']['path']['snapshotId'],
       body: paths['/v1/snapshots/{snapshotId}/permissions']['post']['requestBody']['content']['application/json']
@@ -235,6 +299,15 @@ export const createSnapshotEndpoints = (
       return response.data;
     },
 
+    /**
+     * Revoke access to a shared snapshot.
+     * @param snapshotId The snapshot ID.
+     * @param body The sharing data.
+     * @param body.members The members to revoke access  e.g. `['jane@acme.com', 'john@acme.com']`.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.snapshot.sharing.revoke('123', { members: ['jane@acme.com', 'john@acme.com'] });
+     */
     revoke: async (
       snapshotId: paths['/v1/snapshots/{snapshotId}/permissions']['delete']['parameters']['path']['snapshotId'],
       body: paths['/v1/snapshots/{snapshotId}/permissions']['delete']['requestBody']['content']['application/json']
@@ -258,6 +331,12 @@ export const createSnapshotEndpoints = (
       return response.data;
     },
 
+    /**
+     * List all shared snapshots.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.snapshot.sharing.list();
+     */
     list: async () => {
       const response = await api.GET('/v1/snapshots/shared');
 
