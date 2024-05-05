3876defa35c741ac043e23bf57cfb8789a4475e1
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-04 22:37:21 -0400

    Update device.ts

--- src/device.ts
+++ src/device.ts
@@ -19,7 +19,7 @@ export const createDeviceEndpoints = (
   },
 
   delete: async (instanceId: string) => {
-    const response = await api.DELETE(`/v1/instances/{instanceId}`, {
+    const response = await api.DELETE('/v1/instances/{instanceId}', {
       params: {
         path: {
           instanceId,
@@ -35,7 +35,7 @@ export const createDeviceEndpoints = (
   },
 
   get: async (instanceId: string) => {
-    const response = await api.GET(`/v1/instances/{instanceId}`, {
+    const response = await api.GET('/v1/instances/{instanceId}', {
       params: {
         path: {
           instanceId,
@@ -65,6 +65,106 @@ export const createDeviceEndpoints = (
       params: {
         query: {
           name,
+        },
+      },
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  update: async (
+    instanceId: string,
+    body: paths['/v1/instances/{instanceId}']['patch']['requestBody']['content']['application/json']
+  ) => {
+    const response = await api.PATCH('/v1/instances/{instanceId}', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+      body,
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  start: async (instanceId: string) => {
+    const response = await api.POST('/v1/instances/{instanceId}/start', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  stop: async (instanceId: string) => {
+    const response = await api.POST('/v1/instances/{instanceId}/stop', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  restart: async (instanceId: string) => {
+    const response = await api.POST('/v1/instances/{instanceId}/reboot', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  pause: async (instanceId: string) => {
+    const response = await api.POST('/v1/instances/{instanceId}/pause', {
+      params: {
+        path: {
+          instanceId,
+        },
+      },
+    });
+
+    if (response.error) {
+      throw new Error(response.error.error);
+    }
+
+    return response.data;
+  },
+
+  resume: async (instanceId: string) => {
+    const response = await api.POST('/v1/instances/{instanceId}/unpause', {
+      params: {
+        path: {
+          instanceId,
         },
       },
     });
