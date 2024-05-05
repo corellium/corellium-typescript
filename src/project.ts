378df33a67359a112f96293b65c46b281ba810b3
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-05 12:16:39 -0400

    Add project annotations

--- src/project.ts
+++ src/project.ts
@@ -1,9 +1,15 @@
 import type createFetchClient from 'openapi-fetch';
-import type { paths } from '../types/corellium';
+import type { components, paths } from '../types/corellium';
 
 export const createProjectEndpoints = (
   api: ReturnType<typeof createFetchClient<paths>>
 ) => ({
+  /**
+   * Get a project by ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.project.get('123');
+   */
   get: async (
     projectId: paths['/v1/projects/{projectId}']['get']['parameters']['path']['projectId']
   ) => {
@@ -22,11 +28,43 @@ export const createProjectEndpoints = (
     return response.data;
   },
 
+  /**
+   * Create a new project.
+   * @param body The project settings.
+   * @param body.name The project name.
+   * @param body.settings The project settings.
+   * @param body.settings.internetAccess Whether the project has internet access.
+   * @param body.settings.connection The project connection.
+   * @param body.settings.dhcp Whether the project uses DHCP.
+   * @param body.quotas The project quotas.
+   * @param body.quotas.cores The project core quota.
+   * @param body.quotas.instances The project instance quota.
+   * @param body.quotas.ram The project RAM quota.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.project.create({ name: 'My Project' });
+   */
   create: async (
-    body: paths['/v1/projects']['post']['requestBody']['content']['application/json']
+    body: Omit<
+      paths['/v1/projects']['post']['requestBody']['content']['application/json'],
+      'settings'
+    > & {
+      settings: Omit<
+        components['schemas']['ProjectSettings'],
+        'internet-access'
+      > & {
+        internetAccess: boolean;
+      };
+    }
   ) => {
     const response = await api.POST('/v1/projects', {
-      body,
+      body: {
+        ...body,
+        settings: {
+          ...body.settings,
+          'internet-access': body.settings.internetAccess,
+        },
+      },
     });
 
     if (response.error) {
@@ -36,6 +74,12 @@ export const createProjectEndpoints = (
     return response.data;
   },
 
+  /**
+   * Delete a project by ID.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.project.delete('123');
+   */
   delete: async (
     projectId: paths['/v1/projects/{projectId}']['delete']['parameters']['path']['projectId']
   ) => {
@@ -54,6 +98,12 @@ export const createProjectEndpoints = (
     return response.data;
   },
 
+  /**
+   * List all projects.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.project.list();
+   */
   list: async () => {
     const response = await api.GET('/v1/projects');
 
@@ -64,6 +114,23 @@ export const createProjectEndpoints = (
     return response.data;
   },
 
+  /**
+   * Update a project by ID.
+   * @param projectId The project ID.
+   * @param body The project settings.
+   * @param body.name The project name.
+   * @param body.settings The project settings.
+   * @param body.settings.internetAccess Whether the project has internet access.
+   * @param body.settings.connection The project connection.
+   * @param body.settings.dhcp Whether the project uses DHCP.
+   * @param body.quotas The project quotas.
+   * @param body.quotas.cores The project core quota.
+   * @param body.quotas.instances The project instance quota.
+   * @param body.quotas.ram The project RAM quota.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.project.update('123', { name: 'My Project' });
+   */
   update: async (
     projectId: paths['/v1/projects/{projectId}']['patch']['parameters']['path']['projectId'],
     body: paths['/v1/projects/{projectId}']['patch']['requestBody']['content']['application/json']
@@ -86,6 +153,13 @@ export const createProjectEndpoints = (
   },
 
   device: {
+    /**
+     * List all devices in a project.
+     * @param projectId The project ID.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.project.device.list('123');
+     */
     list: async (
       projectId: paths['/v1/projects/{projectId}/instances']['get']['parameters']['path']['projectId']
     ) => {
@@ -106,6 +180,13 @@ export const createProjectEndpoints = (
   },
 
   vpn: {
+    /**
+     * Get the VPN configuration for a project.
+     * @param projectId The project ID.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.project.vpn.get('123');
+     */
     get: async (
       projectId: paths['/v1/projects/{projectId}/vpnconfig/{format}']['get']['parameters']['path']['projectId']
     ) => {
@@ -130,6 +211,13 @@ export const createProjectEndpoints = (
   },
 
   keys: {
+    /**
+     * List all keys in a project.
+     * @param projectId The project ID.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.project.keys.list('123');
+     */
     list: async (
       projectId: paths['/v1/projects/{projectId}/keys']['get']['parameters']['path']['projectId']
     ) => {
@@ -148,6 +236,17 @@ export const createProjectEndpoints = (
       return response.data;
     },
 
+    /**
+     * Add a key to a project.
+     * @param projectId The project ID.
+     * @param body The key data.
+     * @param body.kind The key kind e.g. `ssh` or `adb`.
+     * @param body.name The key name e.g. `My Key`.
+     * @param body.key The key e.g. `ssh-rsa ...`.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.project.keys.add('123', { kind: 'ssh', name: 'My Key', key: 'ssh-rsa ...' });
+     */
     add: async (
       projectId: paths['/v1/projects/{projectId}/keys']['post']['parameters']['path']['projectId'],
       body: paths['/v1/projects/{projectId}/keys']['post']['requestBody']['content']['application/json']
@@ -168,6 +267,14 @@ export const createProjectEndpoints = (
       return response.data;
     },
 
+    /**
+     * Delete a key from a project.
+     * @param projectId The project ID.
+     * @param keyId The key ID.
+     * @returns The response data.
+     * @throws {Error} The error message.
+     * @example const response = await corellium.project.keys.delete('123', '456');
+     */
     delete: async (
       projectId: paths['/v1/projects/{projectId}/keys/{keyId}']['delete']['parameters']['path']['projectId'],
       keyId: paths['/v1/projects/{projectId}/keys/{keyId}']['delete']['parameters']['path']['keyId']
