aeb0e364873675efadff5a8c9fc14f5d21e33422
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-05 11:57:08 -0400

    Add jsdoc comments for file and hypertrace

--- src/file.ts
+++ src/file.ts
@@ -5,6 +5,13 @@ export const createFileEndpoints = (
   api: ReturnType<typeof createFetchClient<paths>>,
   instanceId: string
 ) => ({
+  /**
+   * Get a file from a device.
+   * @param filePath The path of the file to get e.g. `/etc/hosts`
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').file('/etc/hosts').get();
+   */
   get: async (
     filePath: paths['/v1/instances/{instanceId}/agent/v1/file/device/{filePath}']['get']['parameters']['path']['filePath']
   ) => {
@@ -27,6 +34,14 @@ export const createFileEndpoints = (
     return response.data;
   },
 
+  /**
+   * Create a file on a device.
+   * @param filePath The path of the file to create e.g. `/etc/hosts`
+   * @param body The file content.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').file('/etc/hosts').create('/etc/hosts', File);
+   */
   create: async (
     filePath: paths['/v1/instances/{instanceId}/agent/v1/file/device/{filePath}']['put']['parameters']['path']['filePath'],
     body: paths['/v1/instances/{instanceId}/agent/v1/file/device/{filePath}']['put']['requestBody']['content']['application/octet-stream']
@@ -51,6 +66,13 @@ export const createFileEndpoints = (
     return response.data;
   },
 
+  /**
+   * Delete a file from a device.
+   * @param filePath The path of the file to delete e.g. `/etc/hosts`
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').file('/etc/hosts').delete();
+   */
   delete: async (
     filePath: paths['/v1/instances/{instanceId}/agent/v1/file/device/{filePath}']['delete']['parameters']['path']['filePath']
   ) => {
@@ -73,6 +95,13 @@ export const createFileEndpoints = (
     return response.data;
   },
 
+  /**
+   * Generate a temporary filename.
+   * @description Returns a temporary random filename that is guranteed to be unique on the device's filesystem at the time of invocation of this method.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').file.generateFilename();
+   */
   generateFilename: async () => {
     const response = await api.POST(
       '/v1/instances/{instanceId}/agent/v1/file/temp',
@@ -92,6 +121,15 @@ export const createFileEndpoints = (
     return response.data;
   },
 
+  /**
+   * Update a file on a device.
+   * @param filePath The path of the file to update e.g. `/etc/hosts`
+   * @param body The file content.
+   * @param body.path The new path of the file e.g. `/etc/hosts`
+   * @param body.mode The new mode of the file e.g. `644`
+   * @param body.uid The new owner of the file e.g. `0`
+   * @param body.gid The new group of the file e.g. `0`
+   */
   update: async (
     filePath: paths['/v1/instances/{instanceId}/agent/v1/file/device/{filePath}']['patch']['parameters']['path']['filePath'],
     body: paths['/v1/instances/{instanceId}/agent/v1/file/device/{filePath}']['patch']['requestBody']['content']['application/json']
--- src/hypertrace.ts
+++ src/hypertrace.ts
@@ -5,6 +5,14 @@ export const createHyperTraceEndpoints = (
   api: ReturnType<typeof createFetchClient<paths>>,
   instanceId: string
 ) => ({
+  /**
+   * Start HyperTrace on the device.
+   * @param body The request body.
+   * @param body.ranges The ranges to trace e.g. `['0x1000', '0x2000']`
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').hypertrace.start({ ranges: ['0x1000', '0x2000'] });
+   */
   start: async (
     body: paths['/v1/instances/{instanceId}/btrace/enable']['post']['requestBody']['content']['application/json']
   ) => {
@@ -27,6 +35,12 @@ export const createHyperTraceEndpoints = (
     return response.data;
   },
 
+  /**
+   * Stop HyperTrace on the device.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').hypertrace.stop();
+   */
   stop: async () => {
     const response = await api.POST(
       '/v1/instances/{instanceId}/btrace/disable',
@@ -46,6 +60,12 @@ export const createHyperTraceEndpoints = (
     return response.data;
   },
 
+  /**
+   * Get HyperTrace ranges on the device.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').hypertrace.ranges();
+   */
   ranges: async () => {
     const response = await api.GET(
       '/v1/instances/{instanceId}/btrace-kcrange',
@@ -65,6 +85,12 @@ export const createHyperTraceEndpoints = (
     return response.data;
   },
 
+  /**
+   * Pre-authorize a HyperTrace download.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').hypertrace.authorize();
+   */
   authorize: async () => {
     const response = await api.GET(
       '/v1/instances/{instanceId}/btrace-authorize',
@@ -84,6 +110,12 @@ export const createHyperTraceEndpoints = (
     return response.data;
   },
 
+  /**
+   * Clear HyperTrace logs on the device.
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.device('123').hypertrace.clear();
+   */
   clear: async () => {
     const response = await api.DELETE('/v1/instances/{instanceId}/btrace', {
       params: {
