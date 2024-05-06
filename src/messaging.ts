3c477689c3bf614144bc74e189a8f8e0aa2d0754
Hayden Bleasel <hello@haydenbleasel.com>
2024-05-06 16:06:13 -0400

    Update messaging.ts

--- src/messaging.ts
+++ src/messaging.ts
@@ -5,7 +5,15 @@ export const createMessagingEndpoints = (
   api: ReturnType<typeof createFetchClient<paths>>,
   instanceId: string
 ) => ({
-  // How do you specify the message?
+  /**
+   * Send an SMS message to a device (iOS only)
+   * @param body - The message to send
+   * @param body.number - The phone number to send the message to
+   * @param body.message - The message to send
+   * @returns The response data.
+   * @throws {Error} The error message.
+   * @example const response = await corellium.messaging.receive({ number: '+1234567890', message: 'Hello, World!' });
+   */
   receive: async (
     /*
      * Patch bad OpenAPI spec
